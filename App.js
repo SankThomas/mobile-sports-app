1bf6c35a2c7c13962b527c86f5eb1da04b07198e
sankthomas <thomassankara74@gmail.com>
2024-07-03 22:22:49 +0300

    set up react navigation

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Leagues from "./screens/Leagues";
+import League from "./screens/League";
+import Team from "./screens/Team";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          gestureEnabled: true,
+          animation: "slide_from_right",
+        }}
+      >
+        <Stack.Screen name="Leagues" component={Leagues} />
+        <Stack.Screen name="League" component={League} />
+        <Stack.Screen name="Team" component={Team} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
